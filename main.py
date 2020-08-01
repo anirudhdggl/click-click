56da5908caeacd1b96cdff6a4a18916adad68f66
Anirudh Duggal <anirudhdggl@gmail.com>
2020-08-01 12:05:05 +0530

    main.py

--- main.py
+++ main.py
@@ -3,15 +3,30 @@
 import requests
 from termcolor import colored
 
+# Some websites show forbidden message if
+# user-agent is not present
+
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+
+# The file containig the list of URL
 
 urlFile = "urls.txt"
 urlList = []
 
+# Read all the URLs into a list of URLs
+
 with open(urlFile) as f:
 	urlList = f.readlines()
 
+# Strip off the trailing \n at the end of lines
+# If \n is present, the result will not be correct
+
 urlList = [x.strip() for x in urlList]
+
+# Read headers of all the URLs and check them for
+# X-Frame-Options header. If it's value is set to
+# either SAMEORIGIN or DENY, then it's fine. In any
+# other case, it's vulnerable to clickjacking
 
 for url in urlList:
 	req = requests.head(url)
