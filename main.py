a8e6844546af08c64b2fbb4e3475473c0bda8128
Anirudh Duggal <anirudhdggl@gmail.com>
2020-08-01 12:00:12 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+import requests
+from termcolor import colored
+
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
