10ec621fd08db0e4490b460c35330e324c7a814b
Anirudh Duggal <anirudhdggl@gmail.com>
2020-08-01 12:01:52 +0530

    main.py

--- main.py
+++ main.py
@@ -12,3 +12,17 @@ with open(urlFile) as f:
 	urlList = f.readlines()
 
 urlList = [x.strip() for x in urlList]
+
+for url in urlList:
+	req = requests.head(url)
+	head = req.headers
+	if 'x-frame-options' in head:
+		if head['x-frame-options'] == "SAMEORIGIN" or head['x-frame-options'] == "DENY":
+			safeUrl = colored("[Safe]: " + url,'green')
+			print(safeUrl)
+		else:
+			msg = colored("[" + url + "]: X-frame-options is present but it's value is too permissive","yellow")
+			print(msg)
+	else:
+		vuln = colored("[Vulnerable]: " + url, 'red')
+		print(vuln)
